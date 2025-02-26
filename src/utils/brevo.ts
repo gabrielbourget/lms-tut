90990e873a2c1dca9132131d88d1787c6f6c1f82
Gabriel B <gaboxa@syntronic.com>
2025-02-26 09:48:25 -0500

    Refactor function to be defined separately

--- src/utils/brevo.ts
+++ src/utils/brevo.ts
@@ -10,42 +10,44 @@ const brevoAdapter = (): EmailAdapter => {
     name: "Brevo",
     defaultFromName: BREVO_SENDER_NAME as string,
     defaultFromAddress: BREVO_SENDER_EMAIL as string,
-    sendEmail: async (message: SendEmailOptions): Promise<unknown> => {
-      if (!BREVO_EMAILS_ACTIVE) {
-        console.warn("Email sending is disabled.");
-        return;
-      }
-
-      try {
-        const response = await axios({
-          method: "POST",
-          url: "https://api.brevo.com/v3/smtp/email",
-          headers: {
-            "api-key": BREVO_API_KEY as string,
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-          },
-          data: {
-            sender: {
-              name: BREVO_SENDER_NAME as string,
-              email: BREVO_SENDER_EMAIL as string,
-            },
-            to: [{
-              email: message.to
-            }],
-            subject: message.subject,
-            htmlContent: message.html
-          }
-        });
-
-        return response.data
-      } catch (err) {
-        console.error(`Error while sending email with Brevo -> ${err}`);
-      }
-    }
+    sendEmail
   })
 
   return adapter;
 };
+
+const sendEmail = async (message: SendEmailOptions): Promise<unknown> => {
+  if (!BREVO_EMAILS_ACTIVE) {
+    console.warn("Email sending is disabled.");
+    return;
+  }
+
+  try {
+    const response = await axios({
+      method: "POST",
+      url: "https://api.brevo.com/v3/smtp/email",
+      headers: {
+        "api-key": BREVO_API_KEY as string,
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      data: {
+        sender: {
+          name: BREVO_SENDER_NAME as string,
+          email: BREVO_SENDER_EMAIL as string,
+        },
+        to: [{
+          email: message.to
+        }],
+        subject: message.subject,
+        htmlContent: message.html
+      }
+    });
+
+    return response.data
+  } catch (err) {
+    console.error(`Error while sending email with Brevo -> ${err}`);
+  }
+}
 
 export default brevoAdapter;
